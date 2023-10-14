fe19bd0570b8231eb047d5c3f8640e4e266bb54d
dev-carmine <146045435+dev-carmine@users.noreply.github.com>
2023-10-13 21:52:16 -0700

    Task Score: 66% | Correctness Score: 80%

---
+++ OddOccurrencesInArray/OddOccurrencesInArray.py
@@ -0,0 +1,12 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # Implement your solution here
+    map = {}
+    for num in A:
+        map[num] = map.get(num, 0) + 1
+    
+    for num, count in map.items():
+        if count < 2:
+            return num
